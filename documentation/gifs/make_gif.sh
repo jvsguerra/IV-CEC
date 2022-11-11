01dfe133a2a21ec160f13be3fc3e232697081c47
jvsguerra <jvsguerra@gmail.com>
2022-11-11 18:53:37 -0300

    Add HIV-1 protease gifs

---
+++ documentation/gifs/make_gif.sh
@@ -0,0 +1,8 @@
+# HIV structure MD
+convert -delay 7 -loop 0 HIV/DM/*.png HIV/DM.gif
+
+# HIV Volume/Depth MD
+convert -delay 7 -loop 0 HIV/volume/*.png HIV/volume.gif
+
+# HIV Surface/Hydropathy MD
+convert -delay 7 -loop 0 HIV/surface/*.png HIV/surface.gif
