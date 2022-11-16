cca98e0d2af883f40742a19df435c52f78c58887
jvsguerra <jvsguerra@gmail.com>
2022-11-16 17:34:34 -0300

    Update HIV-1 protease documentation

--- documentation/gifs/make_gif.sh
+++
@@ -1,8 +0,0 @@
-# HIV structure MD
-convert -delay 7 -loop 0 HIV/DM/*.png HIV/DM.gif
-
-# HIV Volume/Depth MD
-convert -delay 7 -loop 0 HIV/volume/*.png HIV/volume.gif
-
-# HIV Surface/Hydropathy MD
-convert -delay 7 -loop 0 HIV/surface/*.png HIV/surface.gif
---
+++ documentation/gifs/make_gifs.sh
@@ -0,0 +1,17 @@
+printf "GIFs for HIV-1 protease"
+
+# HIV structure MD
+printf "> Molecular dynamics\n"
+convert -delay 10 -loop 0 HIV/DM/*.png HIV/DM.gif
+
+# HIV Volume/Depth MD
+printf "> Shape/Volume/Depth characterization\n"
+convert -delay 10 -loop 0 HIV/volume/*.png HIV/volume.gif
+
+# HIV Surface/Hydropathy MD
+printf "> Surface/Hydropathy characeterizaiton\n"
+convert -delay 10 -loop 0 HIV/surface/*.png HIV/surface.gif
+
+# HIV Interface residues MD
+printf "> Constitutional characeterizaiton\n"
+convert -delay 10 -loop 0 HIV/residues/*.png HIV/residues.gif
